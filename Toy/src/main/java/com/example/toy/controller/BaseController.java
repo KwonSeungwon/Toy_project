85f681eb2359ffc682556bf3d0c81baaeb1dbc8f
Seungwon Kwon <loveyoo0103@naver.com>
2021-06-03 17:20:22 +0900

    공통코드 라이브러리 추가
    Signed-off-by: SeungWonKwon <loveyoo0103@naver.com>

--- Toy/src/main/java/com/example/toy/controller/BaseController.java
+++ Toy/src/main/java/com/example/toy/controller/BaseController.java
@@ -1,10 +1,13 @@
 package com.example.toy.controller;
 
 
+import ngineeus.lucia.webs.jpa.UEntities;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import ngineeus.lucia.webs.payload.Payload;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 //@RestController
@@ -12,15 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class BaseController {
 
 
+//    @GetMapping("/")
+//    public ModelAndView home(Payload payload) {
+//        return payload.toModelAndView("home");
+//    }
     @GetMapping("/")
-    public String home() {
-        System.out.println("hello world");
-        return "home";
-    }
-
-    @RequestMapping("/intro")
-    public String introPage() {
-        return "intro";
+    public ModelAndView main(Payload payload) {
+        System.out.println("hhh");
+        return payload.toModelAndView("home");
     }
 
     @GetMapping("/api/v1/move")
